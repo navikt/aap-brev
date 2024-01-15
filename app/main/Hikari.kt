432bf3636813c331bf8624e10efde8835e0335d0
Robin <robin.tordly@nav.no>
2024-01-15 12:33:24 +0100

    log

--- app/main/Hikari.kt
+++ app/main/Hikari.kt
@@ -8,6 +8,7 @@ internal object Hikari {
     private lateinit var datasource: DataSource
 
     fun init(config: PostgresConfig) {
+        SECURE_LOGGER.info("Initializing Hikari")
         datasource = createAndMigrate(config)
     }
 
