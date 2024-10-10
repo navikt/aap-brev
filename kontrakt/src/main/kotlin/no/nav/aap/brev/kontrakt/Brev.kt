5b6579231f0af4162f714c30d98ff31da15676f6
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-10-10 10:50:49 +0200

    Enum for BlokkInnholdType

--- kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Brev.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Brev.kt
@@ -3,6 +3,7 @@ package no.nav.aap.brev.kontrakt
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
+import com.fasterxml.jackson.annotation.JsonValue
 
 data class Brev(
     val overskrift: String?,
@@ -30,23 +31,28 @@ enum class BlokkType {
     AVSNITT, LISTE
 }
 
-const val TEKST_TYPE_TEKST = "TEKST"
-const val TEKST_TYPE_FAKTAGRUNNLAG = "FAKTAGRUNNLAG"
+const val BLOKK_INNHOLD_TYPE_TEKST = "TEKST"
+const val BLOKK_INNHOLD_TYPE_FAKTAGRUNNLAG = "FAKTAGRUNNLAG"
+
+enum class BlokkInnholdType(@JsonValue val verdi: String) {
+
+    TEKST(BLOKK_INNHOLD_TYPE_TEKST), FAKTAGRUNNLAG(BLOKK_INNHOLD_TYPE_FAKTAGRUNNLAG)
+}
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", visible = true)
-sealed class BlokkInnhold(val type: String) {
+sealed class BlokkInnhold(val type: BlokkInnholdType) {
 
-    @JsonTypeName(TEKST_TYPE_TEKST)
+    @JsonTypeName(BLOKK_INNHOLD_TYPE_TEKST)
     data class FormattertTekst(
         val tekst: String,
         val formattering: List<Formattering>,
-    ) : BlokkInnhold(TEKST_TYPE_TEKST)
+    ) : BlokkInnhold(BlokkInnholdType.TEKST)
 
-    @JsonTypeName(TEKST_TYPE_FAKTAGRUNNLAG)
+    @JsonTypeName(BLOKK_INNHOLD_TYPE_FAKTAGRUNNLAG)
     data class Faktagrunnlag(
         val visningsnavn: String,
         val tekniskNavn: String,
-    ) : BlokkInnhold(TEKST_TYPE_FAKTAGRUNNLAG)
+    ) : BlokkInnhold(BlokkInnholdType.FAKTAGRUNNLAG)
 }
 
 enum class Formattering {
