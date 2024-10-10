95a53a316cfdae2521b62a8636e69971b73f932b
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-10-10 14:52:41 +0200

    Utkast til status som skal eksponeres i API'et

---
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Status.kt
@@ -0,0 +1,27 @@
+package no.nav.aap.brev.kontrakt
+
+enum class Status {
+    /**
+     * Brevbestillingen er mottatt og registrert.
+     */
+    REGISTRERT,
+
+    /**
+     * Initiell prosessering, som å hente data fra Sanity,
+     * er utført, og brevet kan redigeres av saksbehandler.
+     */
+    UNDER_ARBEID,
+
+    /**
+     * Brevet er ferdig redigert av saksbehandler og kan
+     * journalføres og distribueres. Eventuelt har bestillingen
+     * gått direkte fra status `REGISTRERT` dersom det ikke er
+     * behov for manuell redigering av saksbehandler.
+     */
+    FERDIGSTILT,
+
+    /**
+     * Brevet er journalført og distribuert.
+     */
+    BESTILT
+}
