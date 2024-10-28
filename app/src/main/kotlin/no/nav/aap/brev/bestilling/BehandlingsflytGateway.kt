40966ad92fa066c692d09a4eafce275fd74eb6d2
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-10-28 14:01:23 +0100

    Legg til mapping av BrevbestillingLøsningStatus

--- app/src/main/kotlin/no/nav/aap/brev/bestilling/BehandlingsflytGateway.kt
+++ app/src/main/kotlin/no/nav/aap/brev/bestilling/BehandlingsflytGateway.kt
@@ -1,10 +1,15 @@
 package no.nav.aap.brev.bestilling
 
+import no.nav.aap.behandlingsflyt.kontrakt.brevbestilling.BrevbestillingLøsningStatus
 import no.nav.aap.brev.kontrakt.Status
 
 class BehandlingsflytGateway : BestillerGateway {
     override fun oppdaterBrevStatus(referanse: BrevbestillingReferanse, status: Status) {
-
+        val brevbestillingLøsningStatus =  when (status) {
+            Status.REGISTRERT, Status.BESTILT -> return
+            Status.UNDER_ARBEID -> BrevbestillingLøsningStatus.KLAR_FOR_EDITERING
+            Status.FERDIGSTILT -> BrevbestillingLøsningStatus.AUTOMATISK_FERDIGSTILT
+        }
     }
 
 }
