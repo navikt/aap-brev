4285716b7cb038e6a7213a123f4a032fab061d5e
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-10-28 10:51:49 +0100

    Midlertidig fortsett ved feil fra Sanity-integrasjon slik at det ikke stopper behandlingsflyten

--- app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/HentInnholdSteg.kt
+++ app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/HentInnholdSteg.kt
@@ -15,10 +15,14 @@ class HentInnholdSteg(
     override fun utfør(kontekst: Steg.Kontekst): Steg.Resultat {
         log.info("Henter brevinnhold for bestillingsreferanse=${kontekst.referanse}")
 
-        val bestilling = brevbestillingRepository.hent(kontekst.referanse)
-        val brev = brevinnholdGateway.hentBrevmal(bestilling.brevtype, bestilling.språk)
+        try { // TODO midlertidig fortsetter vi ved feil slik at det ikke stopper behandlingsflyten
+            val bestilling = brevbestillingRepository.hent(kontekst.referanse)
+            val brev = brevinnholdGateway.hentBrevmal(bestilling.brevtype, bestilling.språk)
 
-        brevbestillingRepository.oppdaterBrev(bestilling.referanse, brev)
+            brevbestillingRepository.oppdaterBrev(bestilling.referanse, brev)
+        } catch (e: Exception) {
+            log.error("Feil under henting av brevmal", e)
+        }
 
         return Steg.Resultat.FULLFØRT
     }
