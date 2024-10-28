f9a9b4406b967cf851481ee422a3c69620abf12c
Henrik Jackson <henrik.m.jackson@gmail.com>
2024-10-28 15:24:01 +0100

    Opprett kontakt med behandlingsflyt

--- app/src/main/kotlin/no/nav/aap/brev/bestilling/BehandlingsflytGateway.kt
+++ app/src/main/kotlin/no/nav/aap/brev/bestilling/BehandlingsflytGateway.kt
@@ -1,15 +1,46 @@
 package no.nav.aap.brev.bestilling
 
 import no.nav.aap.behandlingsflyt.kontrakt.brevbestilling.BrevbestillingLøsningStatus
+import no.nav.aap.behandlingsflyt.kontrakt.brevbestilling.LøsBrevbestillingDto
 import no.nav.aap.brev.kontrakt.Status
+import no.nav.aap.komponenter.config.requiredConfigForKey
+import no.nav.aap.komponenter.httpklient.httpclient.ClientConfig
+import no.nav.aap.komponenter.httpklient.httpclient.Header
+import no.nav.aap.komponenter.httpklient.httpclient.RestClient
+import no.nav.aap.komponenter.httpklient.httpclient.post
+import no.nav.aap.komponenter.httpklient.httpclient.request.PostRequest
+import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.azurecc.ClientCredentialsTokenProvider
+import java.net.URI
 
 class BehandlingsflytGateway : BestillerGateway {
+    private val baseUri = URI.create(requiredConfigForKey("integrasjon.behandlingsflyt.url"))
+    val config = ClientConfig(scope = requiredConfigForKey("integrasjon.behandlingsflyt.scope"))
+    private val client = RestClient.withDefaultResponseHandler(
+        config = config,
+        tokenProvider = ClientCredentialsTokenProvider
+    )
+
     override fun oppdaterBrevStatus(referanse: BrevbestillingReferanse, status: Status) {
-        val brevbestillingLøsningStatus =  when (status) {
+        val brevbestillingLøsningStatus = when (status) {
             Status.REGISTRERT -> return
             Status.UNDER_ARBEID -> BrevbestillingLøsningStatus.KLAR_FOR_EDITERING
             Status.FERDIGSTILT -> BrevbestillingLøsningStatus.AUTOMATISK_FERDIGSTILT
         }
+
+        val request = LøsBrevbestillingDto(
+            referanse = referanse.referanse,
+            brevbestillingLøsningStatus,
+        )
+
+        val uri = baseUri.resolve("/api/brev/løs-bestilling")
+        val httpRequest = PostRequest(
+            body = request,
+            additionalHeaders = listOf(
+                Header("Accept", "application/json")
+            )
+        )
+
+        client.post<_, Unit>(uri = uri, request = httpRequest)
     }
 
 }
--- app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/LøsBrevbestillingSteg.kt
+++ app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/LøsBrevbestillingSteg.kt
@@ -1,19 +1,24 @@
 package no.nav.aap.brev.prosessering.steg
 
+import no.nav.aap.brev.bestilling.BehandlingsflytGateway
+import no.nav.aap.brev.bestilling.BestillerGateway
+import no.nav.aap.brev.kontrakt.Status
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import org.slf4j.LoggerFactory
 
-class LøsBrevbestillingSteg  : Steg.Utfører {
+class LøsBrevbestillingSteg(val bestillerGateway: BestillerGateway) : Steg.Utfører {
     private val log = LoggerFactory.getLogger(LøsBrevbestillingSteg::class.java)
     override fun utfør(kontekst: Steg.Kontekst): Steg.Resultat {
         log.info("LøsBrevbestillingSteg")
-        // TODO løs brevbestilling mot behandlingsflyt
+
+        // TODO: Sende riktig status i oppdaterBrevStatus - Er Status.FERDIGSTILT for å forhindre stopp i flyten
+        bestillerGateway.oppdaterBrevStatus(kontekst.referanse, Status.FERDIGSTILT)
         return Steg.Resultat.FULLFØRT
     }
 
     companion object : Steg {
         override fun konstruer(connection: DBConnection): Steg.Utfører {
-            return LøsBrevbestillingSteg()
+            return LøsBrevbestillingSteg(bestillerGateway = BehandlingsflytGateway())
         }
     }
 }
