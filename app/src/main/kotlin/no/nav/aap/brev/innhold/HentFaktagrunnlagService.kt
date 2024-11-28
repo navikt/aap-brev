348086fb466a1ee1858bfe332faa34e5c982ab98
Henrik Jackson <henrik.m.jackson@gmail.com>
2024-11-28 13:37:28 +0100

    Map om faktagrunnlag til formattert tekst
    Co-authored-by: Matias Vinjevoll <matias.vinjevoll@nav.no>

--- app/src/main/kotlin/no/nav/aap/brev/innhold/HentFaktagrunnlagService.kt
+++ app/src/main/kotlin/no/nav/aap/brev/innhold/HentFaktagrunnlagService.kt
@@ -11,6 +11,7 @@ import no.nav.aap.brev.kontrakt.BlokkInnhold
 import no.nav.aap.brev.kontrakt.Brev
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class HentFaktagrunnlagService(
     private val hentFagtagrunnlagGateway: HentFagtagrunnlagGateway,
@@ -38,12 +39,20 @@ class HentFaktagrunnlagService(
         brevbestillingRepository.oppdaterBrev(bestilling.referanse, oppdatertBrev)
     }
 
-    private fun erstattFaktagrunnlag(brev: Brev, foo: Set<Faktagrunnlag>): Brev {
-        TODO("Not yet implemented")
-    }
+    private fun erstattFaktagrunnlag(brev: Brev, faktagrunnlag: Set<Faktagrunnlag>): Brev =
+        brev.copy(tekstbolker = brev.tekstbolker.map { tekstbolk ->
+            tekstbolk.copy(innhold = tekstbolk.innhold.map { innhold ->
+                innhold.copy(blokker = innhold.blokker.map { blokk ->
+                    blokk.copy(innhold = blokk.innhold.map { blokkInnhold ->
+                        mapBlokkInnholdTilTekst(
+                            blokkInnhold,
+                            faktagrunnlag
+                        )
+                    })
+                })
+            })
+        })
 
-    private fun mapFaktagrunnlag(faktagrunnlag: BlokkInnhold.Faktagrunnlag): FaktagrunnlagType? =
-        FaktagrunnlagType.entries.find { it.name == faktagrunnlag.tekniskNavn.uppercase() }
 
     private fun finnFaktagrunnlag(brev: Brev): List<BlokkInnhold.Faktagrunnlag> =
         brev.tekstbolker
@@ -52,6 +61,38 @@ class HentFaktagrunnlagService(
             .flatMap { it.innhold }
             .filterIsInstance<BlokkInnhold.Faktagrunnlag>()
 
+    private fun mapBlokkInnholdTilTekst(blokkInnhold: BlokkInnhold, faktagrunnlag: Set<Faktagrunnlag>): BlokkInnhold =
+        when (blokkInnhold) {
+            is BlokkInnhold.FormattertTekst -> blokkInnhold
+            is BlokkInnhold.Faktagrunnlag ->
+                mapFaktagrunnlag(blokkInnhold)
+                    ?.let { finnFaktagrunnlad(faktagrunnlag, it) }
+                    ?.let { faktagrunnlagTilFormatertTekst(it, blokkInnhold) }
+                    ?: blokkInnhold
+        }
+
+    private fun mapFaktagrunnlag(blokkInnhold: BlokkInnhold.Faktagrunnlag): FaktagrunnlagType? =
+        FaktagrunnlagType.entries.find { it.name == blokkInnhold.tekniskNavn.uppercase() }
+
+    private fun finnFaktagrunnlad(
+        faktagrunnlag: Set<Faktagrunnlag>,
+        faktagrunnlagType: FaktagrunnlagType
+    ): Faktagrunnlag? =
+        faktagrunnlag.find { it.type == faktagrunnlagType }
+
+    private fun faktagrunnlagTilFormatertTekst(
+        faktagrunnlag: Faktagrunnlag,
+        blokkInnhold: BlokkInnhold.Faktagrunnlag
+    ): BlokkInnhold.FormattertTekst {
+        return when (faktagrunnlag) {
+            is Faktagrunnlag.Startdato ->
+                BlokkInnhold.FormattertTekst(
+                    id = blokkInnhold.id,
+                    tekst = faktagrunnlag.dato.format(DateTimeFormatter.ofPattern("dd.mm.yyyy")),
+                    formattering = emptyList(),
+                )
+        }
+    }
 }
 
 // TODO Flytt alt under til Behandlingsflyt-kontrakt
