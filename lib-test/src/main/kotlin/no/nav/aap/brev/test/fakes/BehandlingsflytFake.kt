de2ba19e490be1c2467d3f5e9e5895c3b42effb2
Henrik Jackson <henrik.m.jackson@gmail.com>
2024-11-28 14:44:53 +0100

    Oppdater behandlingsflytFake med kall mot endepunkt for å hente faktagrunnlag for behandlingsreferanse
    Co-authored-by: Matias Vinjevoll <matias.vinjevoll@nav.no>

--- lib-test/src/main/kotlin/no/nav/aap/brev/test/fakes/BehandlingsflytFake.kt
+++ lib-test/src/main/kotlin/no/nav/aap/brev/test/fakes/BehandlingsflytFake.kt
@@ -6,14 +6,24 @@ import io.ktor.server.request.receiveText
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.aap.behandlingsflyt.kontrakt.brevbestilling.LøsBrevbestillingDto
+import no.nav.aap.brev.bestilling.BehandlingReferanse
+import no.nav.aap.brev.bestilling.BehandlingsflytGateway
 import no.nav.aap.brev.bestilling.BrevbestillingReferanse
 import no.nav.aap.brev.bestilling.Personinfo
+import no.nav.aap.brev.innhold.Faktagrunnlag
 import no.nav.aap.komponenter.httpklient.json.DefaultJsonMapper
 import java.util.UUID
 
 private val feilForBestilling = mutableSetOf<UUID>()
+
+private val behandlingsReferanseTilFaktagrunnlag = mutableMapOf<BehandlingReferanse, Set<Faktagrunnlag>>()
+
 fun feilLøsBestillingFor(bestilling: BrevbestillingReferanse) {
     feilForBestilling.add(bestilling.referanse)
+}
+
+fun faktagrunnlagForBehandling(behandlingReferanse: BehandlingReferanse, faktagrunnlag: Set<Faktagrunnlag>) {
+    behandlingsReferanseTilFaktagrunnlag.put(behandlingReferanse, faktagrunnlag)
 }
 
 fun Application.behandlingsflytFake() {
@@ -28,6 +38,13 @@ fun Application.behandlingsflytFake() {
         }
         get("/api/sak/{saksnummer}/personinformasjon") {
             call.respond(Personinfo("", ""))
+        }
+        post("/api/brev/faktagrunnlag") {
+            val dto = DefaultJsonMapper.fromJson<BehandlingsflytGateway.HentFaktaGrunnlagRequest>(call.receiveText())
+
+            val faktagrunnlagResult = behandlingsReferanseTilFaktagrunnlag.get(dto.behandlingReferanse) ?: emptySet()
+
+            call.respond(BehandlingsflytGateway.HentFaktaGrunnlagResponse(faktagrunnlagResult))
         }
     }
 }
