811a7877a591860b53e1e19ece01e4fb658aff2d
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-11-29 09:30:40 +0100

    Distribuer journalpost

--- app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/DistribuerJournalpostSteg.kt
+++ app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/DistribuerJournalpostSteg.kt
@@ -1,18 +1,20 @@
 package no.nav.aap.brev.prosessering.steg
 
+import no.nav.aap.brev.distribusjon.DistribusjonService
 import no.nav.aap.komponenter.dbconnect.DBConnection
 import org.slf4j.LoggerFactory
 
-class DistribuerJournalpostSteg() : Steg.Utfører {
+class DistribuerJournalpostSteg(val distribusjonService: DistribusjonService) : Steg.Utfører {
     private val log = LoggerFactory.getLogger(DistribuerJournalpostSteg::class.java)
     override fun utfør(kontekst: Steg.Kontekst): Steg.Resultat {
         log.info("DistribuerJournalpostSteg")
+        distribusjonService.distribuerBrev(kontekst.referanse)
         return Steg.Resultat.FULLFØRT
     }
 
     companion object : Steg {
         override fun konstruer(connection: DBConnection): Steg.Utfører {
-            return DistribuerJournalpostSteg()
+            return DistribuerJournalpostSteg(DistribusjonService.konstruer(connection))
         }
     }
 }
