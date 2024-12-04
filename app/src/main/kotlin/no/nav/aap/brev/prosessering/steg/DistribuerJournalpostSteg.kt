0b281529cd09677b95201177d0f0d79c21ab1f0a
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-12-04 11:03:02 +0100

    Skru på distribusjon av journalpost

--- app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/DistribuerJournalpostSteg.kt
+++ app/src/main/kotlin/no/nav/aap/brev/prosessering/steg/DistribuerJournalpostSteg.kt
@@ -2,17 +2,13 @@ package no.nav.aap.brev.prosessering.steg
 
 import no.nav.aap.brev.distribusjon.DistribusjonService
 import no.nav.aap.komponenter.dbconnect.DBConnection
-import no.nav.aap.komponenter.miljo.Miljø
-import no.nav.aap.komponenter.miljo.MiljøKode
 import org.slf4j.LoggerFactory
 
 class DistribuerJournalpostSteg(val distribusjonService: DistribusjonService) : Steg.Utfører {
     private val log = LoggerFactory.getLogger(DistribuerJournalpostSteg::class.java)
     override fun utfør(kontekst: Steg.Kontekst): Steg.Resultat {
         log.info("DistribuerJournalpostSteg")
-        if (Miljø.er() != MiljøKode.DEV) {
-            distribusjonService.distribuerBrev(kontekst.referanse)
-        }
+        distribusjonService.distribuerBrev(kontekst.referanse)
         return Steg.Resultat.FULLFØRT
     }
 
