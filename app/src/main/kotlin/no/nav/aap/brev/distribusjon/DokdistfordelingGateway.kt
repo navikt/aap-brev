f4649cd73cf57079b5020c405d7ece77e20ca3f1
Matias Vinjevoll <matias.vinjevoll@nav.no>
2024-12-18 11:18:35 +0100

    Legg til brevtype VARSEL_OM_BESTILLING
    Co-authored-by: Henrik Bugge Halvorsen <Henrik.Bugge.Halvorsen@nav.no>

--- app/src/main/kotlin/no/nav/aap/brev/distribusjon/DokdistfordelingGateway.kt
+++ app/src/main/kotlin/no/nav/aap/brev/distribusjon/DokdistfordelingGateway.kt
@@ -41,6 +41,7 @@ class DokdistfordelingGateway : DistribusjonGateway {
     private fun utledDistribusjonstype(brevtype: Brevtype): Distribusjonstype {
         return when (brevtype) {
             Brevtype.INNVILGELSE, Brevtype.AVSLAG -> Distribusjonstype.VEDTAK
+            Brevtype.VARSEL_OM_BESTILLING -> Distribusjonstype.ANNET // TODO, er det riktig?
         }
     }
 }
--- kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Brevtype.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Brevtype.kt
@@ -3,4 +3,5 @@ package no.nav.aap.brev.kontrakt
 enum class Brevtype {
     INNVILGELSE,
     AVSLAG,
+    VARSEL_OM_BESTILLING
 }
