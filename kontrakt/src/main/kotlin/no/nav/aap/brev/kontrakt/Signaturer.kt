21380bd9f6f7b84478e815a1866ef19bdf2c5f3f
Matias Vinjevoll <matias.vinjevoll@nav.no>
2025-04-01 15:49:35 +0200

    Legg til optional rolle i signatur grunnlag
    Co-authored-by: Selma Kristine Bergstrand <Selma.Kristine.Bergstrand@nav.no>

--- kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Signaturer.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/Signaturer.kt
@@ -1,5 +1,12 @@
 package no.nav.aap.brev.kontrakt
 
-data class SignaturGrunnlag(val navIdent: String)
+data class SignaturGrunnlag(val navIdent: String, val rolle: Rolle?)
 
 data class Signatur(val navn: String, val enhet: String)
+
+enum class Rolle {
+    KVALITETSSIKRER,
+    SAKSBEHANDLER_OPPFOLGING,
+    BESLUTTER,
+    SAKSBEHANDLER_NASJONAL,
+}
