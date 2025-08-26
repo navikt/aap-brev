5aaef899f0eafa7e39faa976e5c74adc2dedee12
Matias Vinjevoll <1862220+matiasvinjevoll@users.noreply.github.com>
2025-08-26 14:40:32 +0200

    Bruk rounding mode HALF_UP for tall i brevet (#359)

--- app/src/main/kotlin/no/nav/aap/brev/util/NumberUtils.kt
+++ app/src/main/kotlin/no/nav/aap/brev/util/NumberUtils.kt
@@ -2,12 +2,14 @@ package no.nav.aap.brev.util
 
 import no.nav.aap.brev.kontrakt.Språk
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.text.DecimalFormat
 
 object NumberUtils {
     fun BigDecimal.formater(språk: Språk): String {
         val decimalFormat = DecimalFormat.getInstance(språk.toLocale())
         decimalFormat.maximumFractionDigits = 2
+        decimalFormat.roundingMode = RoundingMode.HALF_UP
         return decimalFormat.format(this)
     }
 }
