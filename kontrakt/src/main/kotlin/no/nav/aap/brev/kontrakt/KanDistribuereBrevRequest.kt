7dea44bee0f246838feae763ef2dcc6f33c1a7a5
Greger Siem Knudsen <149553638+GregerSKnudsen@users.noreply.github.com>
2025-11-05 16:55:08 +0100

    Korrigerte til data class i KanDistribuereBrevRequest.kt

--- kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/KanDistribuereBrevRequest.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/KanDistribuereBrevRequest.kt
@@ -1,6 +1,6 @@
 package no.nav.aap.brev.kontrakt
 
-class KanDistribuereBrevRequest (
+data class KanDistribuereBrevRequest (
     val brukerIdent: String,
     val mottakerIdentListe: List<String> = emptyList(),
 )
