a80bfeff0f166143cf1b6c6a719ab3b70c9c53e8
Greger Siem Knudsen <greger.siem.knudsen@nav.no>
2025-11-14 14:45:16 +0100

    Fjernet overflødig autorisasjonsparam

--- kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/KanDistribuereBrevRequest.kt
+++ kontrakt/src/main/kotlin/no/nav/aap/brev/kontrakt/KanDistribuereBrevRequest.kt
@@ -1,7 +1,6 @@
 package no.nav.aap.brev.kontrakt
 
 data class KanDistribuereBrevRequest (
-    val behandlingsReferanse: String,
     val brukerIdent: String,
     val mottakerIdentListe: List<String> = emptyList(),
 )
