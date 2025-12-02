08608c8839e765931702c837cee8c512d03558d7
Øivind Stensrud <oivind.stensrud@nav.no>
2025-12-02 13:03:57 +0100

    Legger til litt logging for feilsøking
    Co-authored-by: Joacim Thomassen <joacim.thomassen@nav.no>

--- app/src/main/kotlin/no/nav/aap/brev/bestilling/BrevmalJson.kt
+++ app/src/main/kotlin/no/nav/aap/brev/bestilling/BrevmalJson.kt
@@ -1,11 +1,16 @@
 package no.nav.aap.brev.bestilling
 
 import com.fasterxml.jackson.databind.node.ObjectNode
+import no.nav.aap.brev.App
 import no.nav.aap.komponenter.json.DefaultJsonMapper
+import org.slf4j.LoggerFactory
+
+private val LOGGER = LoggerFactory.getLogger(App::class.java)
 
 @JvmInline
 value class BrevmalJson(val json: ObjectNode) {
     fun tilBrevmal(): Brevmal {
+        LOGGER.info("BrevmalJson: $json")
         return DefaultJsonMapper.objectMapper().treeToValue(json, Brevmal::class.java)
     }
 }
