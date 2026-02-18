daf00dfdb7540744bd131305028ab46a71a19978
Matias Vinjevoll <1862220+matiasvinjevoll@users.noreply.github.com>
2026-02-18 09:42:21 +0100

    Fjern deprecation på ValideringsfeilException (#582)
    ValideringsfeilException er til bruk i domenelogikk. Kan oversettes til ApiException i StatusPages. Burde ideelt sett vært forskjellige moduler (domene, api).

--- app/src/main/kotlin/no/nav/aap/brev/feil/ValideringsfeilException.kt
+++ app/src/main/kotlin/no/nav/aap/brev/feil/ValideringsfeilException.kt
@@ -1,7 +1,3 @@
 package no.nav.aap.brev.feil
 
-@Deprecated(
-    message = "Bruk heller en variant av ApiException (UgyldigForespørselException, Internfeil, etc.)",
-    replaceWith = ReplaceWith("no.nav.aap.komponenter.httpklient.exception.ApiException")
-)
 class ValideringsfeilException(message: String): IllegalStateException(message)
