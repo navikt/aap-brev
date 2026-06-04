4bf933b632bac31ed3fc6348c1db45adaf997a5d
Hein Haraldsen <hein.haraldsen@nav.no>
2026-06-04 12:44:21 +0200

    Retter på antall barn uten barnetillegg

--- app/src/main/kotlin/no/nav/aap/brev/innhold/FaktagrunnlagService.kt
+++ app/src/main/kotlin/no/nav/aap/brev/innhold/FaktagrunnlagService.kt
@@ -182,24 +182,9 @@ class FaktagrunnlagService(
                     }
 
                     is Faktagrunnlag.BarnUtenBarnetillegg -> {
-                        val barnUtenForeldreAnsvar = faktagrunnlag.barn.filter { !it.harForeldreAnsvar }
-                        if (barnUtenForeldreAnsvar.isNotEmpty()) {
-                            put(
-                                KjentFaktagrunnlag.ANTALL_BARN_UTEN_BARNETILLEGG,
-                                barnUtenForeldreAnsvar.joinToString(separator = "\n") { barnUtenBarnetilleggTekst(it) }
-                            )
                         put(
                             KjentFaktagrunnlag.ANTALL_BARN_UTEN_BARNETILLEGG, faktagrunnlag.barn.toString()
                         )
-                    }
-
-
-
-                    is Faktagrunnlag.ForeldreAnsvar ->
-                    {
-                        faktagrunnlag.erFosterforelder?.let { erFosterforelder ->
-                            put(KjentFaktagrunnlag.ER_FOSTERFORELDER, erFosterforelder.toString())
-                        }
                     }
 
                     else -> {}
