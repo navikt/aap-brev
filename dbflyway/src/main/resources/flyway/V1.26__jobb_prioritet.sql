1693028fdcf95c643bdfddf273a330a1f9619196
charliemidtlyng <charlie.midtlyng@nav.no>
2026-08-18 10:38:19 +0200

    Skal ha riktig status - caps

--- dbflyway/src/main/resources/flyway/V1.26__jobb_prioritet.sql
+++ dbflyway/src/main/resources/flyway/V1.26__jobb_prioritet.sql
@@ -2,4 +2,4 @@ alter table jobb add column prioritet smallint not null default 100;
 alter table jobb add column kjorbar boolean not null default false;
 
 create index idx_jobb_plukkbar_prioritet on jobb (prioritet, neste_kjoring)
-    where status = 'klar' and kjorbar;
+    where status = 'KLAR' and kjorbar;
