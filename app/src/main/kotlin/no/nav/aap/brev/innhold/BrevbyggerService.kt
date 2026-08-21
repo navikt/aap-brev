7a4a4321886cf560515e57c10be164e678ed7f26
larsgrini <lars.strande.grini@nav.no>
2026-08-21 09:42:50 +0200

    aap-2324: legge til årsaker for vurdering av tidspunkt ytterligere nedsatt

--- app/src/main/kotlin/no/nav/aap/brev/innhold/BrevbyggerService.kt
+++ app/src/main/kotlin/no/nav/aap/brev/innhold/BrevbyggerService.kt
@@ -16,6 +16,9 @@ import no.nav.aap.brev.innhold.KjentKategori.HAR_SAMORDNING_UFØRE
 import no.nav.aap.brev.innhold.KjentKategori.HAR_SYKESTIPEND
 import no.nav.aap.brev.kontrakt.Brevtype
 import no.nav.aap.brev.kontrakt.Faktagrunnlag
+import no.nav.aap.brev.kontrakt.Faktagrunnlag.AarsakTidspunktVurdering
+import no.nav.aap.brev.kontrakt.Faktagrunnlag.AarsakTidspunktVurdering.AarsakBeregningstidspunkt
+import no.nav.aap.brev.kontrakt.Faktagrunnlag.AarsakTidspunktVurdering.AarsakYtterligereNedsatt
 import no.nav.aap.brev.kontrakt.Faktagrunnlag.ForholdTilAndreYtelser
 import no.nav.aap.brev.kontrakt.Faktagrunnlag.GrunnlagBeregning
 import no.nav.aap.brev.kontrakt.Faktagrunnlag.GrunnlagBeregning.BeregningsutfallKategori
@@ -193,7 +196,36 @@ class BrevbyggerService(
                     }
                 }
 
-                else -> emptySet()
+                is AarsakTidspunktVurdering -> {
+                    val årsak = buildSet {
+                        when (faktagrunnlag.aarsakBeregningsTidspunktVurdering) {
+                            AarsakBeregningstidspunkt.SEKSTEN_AAR_SOM_BEREGNINGSTIDSPUNKT -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_SEKSTEN_ÅR_SOM_BEREGNINGSTIDSPUNKT)
+                            AarsakBeregningstidspunkt.UFOERETIDSPUNKT -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_UFØRETIDSPUNKT)
+                            AarsakBeregningstidspunkt.SYKEMELDINGSDATO -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_SYKEMELDINGSDATO)
+                            AarsakBeregningstidspunkt.HENVIST_TIL_BEHANDLING -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_HENVIST_TIL_BEHANDLING)
+                            AarsakBeregningstidspunkt.DATO_PAA_LEGEERKLAERING -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_DATO_PAA_LEGEERKLÆRING)
+                            AarsakBeregningstidspunkt.KRAVDATO -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_KRAVDATO)
+                            AarsakBeregningstidspunkt.ANNET -> add(KjentKategori.AARSAK_BEREGNINGSTIDSPUNKT_ANNET)
+                            null -> {}
+                        }
+                    }
+                    val nedsatt = buildSet {
+                        when(faktagrunnlag.aarsakYtterligereNedsattTidspunktVurdering){
+                            AarsakYtterligereNedsatt.UFOERETIDSPUNKT -> add(KjentKategori.AARSAK_YTTERLIGERE_NEDSATT_UFOERETIDSPUNKT)
+                            AarsakYtterligereNedsatt.YTTERLIGERE_NEDSATT -> add(KjentKategori.AARSAK_YTTERLIGERE_NEDSATT_YTTERLIGERE_NEDSATT)
+                            AarsakYtterligereNedsatt.OKT_UFOEREGRAD -> add(KjentKategori.AARSAK_YTTERLIGERE_NEDSATT_OKT_UFOEREGRAD)
+                            AarsakYtterligereNedsatt.IKKE_BETYDNING_IKKE_RELEVANT -> add(KjentKategori.AARSAK_YTTERLIGERE_NEDSATT_IKKE_BETYDNING_IKKE_RELEVANT)
+                            AarsakYtterligereNedsatt.ANNET -> add(KjentKategori.AARSAK_YTTERLIGERE_NEDSATT_ANNET)
+                            null -> {}
+                        }
+                    }
+
+                    årsak + nedsatt
+                }
+
+                else -> {
+                    emptySet()
+                }
             }
         }.toSet()
 
